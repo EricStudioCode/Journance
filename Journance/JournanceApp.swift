02e2981f8d6baf496da38ac5a7aee709af5c223e
Eric <ericaxt04@gmail.com>
2023-09-28 12:07:14 +0200

    Initial Commit

---
+++ Journance/JournanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournanceApp.swift
+//  Journance
+//
+//  Created by Eric  on 28.09.23.
+//
+
+import SwiftUI
+
+@main
+struct JournanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
